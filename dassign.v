f530192fc1a7198f50c130b7e8eaff707b8f98ac
Ari Sweedler <ari@sweedler.com>
2019-03-02 11:21:35 -0800

    15m - First commit.
      * Read the spec, and set up the skeleton for the 'game' module.

---
+++ dassign.v
@@ -0,0 +1,12 @@
+module game(turnX, turnO, occ_pos, game_st, reset, clk, flash_clk, sel_pos, buttonX, buttonO)
+  output turnX, turn0;
+  output occ_pos[8:0];
+  output game_st[7:0];
+
+  input reset, clk, flash_clk;
+  input sel_pos[8:0];
+  input buttonX, buttonO;
+
+  /**************************** begin game logic *****************************/
+
+endmodule
