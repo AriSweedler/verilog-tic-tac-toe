8a1464066434f3ffde1f1dc892ee3cafe46ce759
Ari Sweedler <ari@sweedler.com>
2019-04-10 16:49:39 -0700

    Reformatted/fixed syntax
      * Find it here: https://www.edaplayground.com/x/5jwx

--- dassign.v
+++ dassign.v
@@ -1,6 +1,10 @@
-/* For my internal state, X is 1, O is 0 */
+`timescale 1ns / 1ps
 
-//TODO we can be more clever with our state scheme later
+/* For my internal state, X is 1, O is 0 */
+`define X_TILE 1'b1
+`define O_TILE 1'b0
+
+/* Game states bit patterns */
 `define GAME_ST_START 4'd0
 `define GAME_ST_TURN_X 4'd1
 `define GAME_ST_ERR_X 4'd2
@@ -11,26 +15,39 @@
 `define GAME_ST_CHECK_O 4'd7
 `define GAME_ST_WIN_O 4'd8
 `define GAME_ST_CATS 4'd9
+
+/* Win state bit patterns */
+`define RESULT_NONE 2'd0
+`define RESULT_CATS 2'd1
+`define RESULT_WINO 2'd2
+`define RESULT_WINX 2'd3
+
 module game(turnX, turnO, occ_pos, game_st, reset, clk, flash_clk, sel_pos, buttonX, buttonO);
-  output turnX, turn0;
-  output occ_pos[8:0];
-  output game_st[7:0];
+  output turnX;
+  output turnO;
+  output [8:0] occ_pos;
+  output [7:0] game_st;
 
   input reset, clk, flash_clk;
-  input sel_pos[8:0];
+  input [8:0] sel_pos;
   input buttonX, buttonO;
 
-  wire occ_square[8:0];
-  wire occ_player[8:0];
-  wire game_state[3:0];
+  /* occ_square states if there's a tile in this square or not */
+  reg [8:0] occ_square;
+  /* occ_player states which type of tile is in the square */
+  reg [8:0] occ_player;
+  reg [3:0] game_state;
 
   wire valid_move;
   check_valid_move cvm (valid_move, occ_square, sel_pos);
 
-  debouncer db_buttonX (buttonX_debounce, buttonX, clk, rst);
-  debouncer db_buttonO (buttonO_debounce, buttonO, clk, rst);
+  wire buttonX_debounce, buttonO_debounce;
+  debouncer db_buttonX (buttonX_debounce, buttonX, clk, reset);
+  debouncer db_buttonO (buttonO_debounce, buttonO, clk, reset);
 
-  game_st_driver _game_st_driver (game_st, game_state);
+  wire [7:0] game_st_ascii;
+  assign game_st = game_st_ascii;
+  game_st_driver _game_st_driver (game_st_ascii, game_state);
 
   /* asynchronous resets */
   always @ (posedge clk, posedge reset) begin
@@ -38,44 +55,52 @@ module game(turnX, turnO, occ_pos, game_st, reset, clk, flash_clk, sel_pos, butt
       occ_square <= 9'b000000000;
       occ_player <= 9'b000000000;
       game_state <= `GAME_ST_START;
-    else begin
+    end else begin
       if (game_state == `GAME_ST_START) begin
         game_state <= `GAME_ST_TURN_X;
       end else if (game_state == `GAME_ST_TURN_X) begin
-        if (buttonX_debounce &valid_move) begin
-          occ_square <= (occ_square) | (sel_pos); //mark this square as occupied
-          occ_player <= (occ_player) | (sel_pos); //set the selected position to high (X)
+        if (buttonX_debounce & valid_move) begin
+          /* mark this square as occupied */
+          occ_square <= (occ_square) | (sel_pos);
+          /* set the selected position to be an X_TILE (1/high) */
+          occ_player <= (occ_player) | (sel_pos);
         end else if ((buttonX_debounce | buttonO_debounce) & ~valid_move) begin
-          game_state == `GAME_ST_ERR_X;
+          game_state <= `GAME_ST_ERR_X;
         end else game_state <= game_state;
       end else if (game_state == `GAME_ST_TURN_O) begin
-        if (buttonO_debounce &valid_move) begin
-          occ_square <= (occ_square) | (sel_pos); //mark this square as occupied
-          occ_player <= (occ_player) & (~sel_pos); //set the selected position to low (O)
+        if (buttonO_debounce & valid_move) begin
+          /* mark this square as occupied */
+          occ_square <= (occ_square) | (sel_pos);
+          /* set the selected position to be an O_TILE (0/low) */
+          occ_player <= (occ_player) & (~sel_pos);
         end else if ((buttonX_debounce | buttonO_debounce) & ~valid_move) begin
-          game_state == `GAME_ST_ERR_O;
+          game_state <= `GAME_ST_ERR_O;
         end else game_state <= game_state;
       end else if (game_state == `GAME_ST_ERR_X) begin
         if (buttonX_debounce & valid_move) begin
-          occ_square <= (occ_square) | (sel_pos); //mark this square as occupied
-          occ_player <= (occ_player) | (sel_pos); //set the selected position to high (X)
+          /* mark this square as occupied */
+          occ_square <= (occ_square) | (sel_pos);
+          /* set the selected position to be an X_TILE (1/high) */
+          occ_player <= (occ_player) | (sel_pos);
         end else game_state <= game_state;
       end else if (game_state == `GAME_ST_ERR_O) begin
         if (buttonO_debounce & valid_move) begin
-          occ_square <= (occ_square) | (sel_pos); //mark this square as occupied
-          occ_player <= (occ_player) & (~sel_pos); //set the selected position to low (O)
+          /* mark this square as occupied */
+          occ_square <= (occ_square) | (sel_pos);
+          /* set the selected position to be an O_TILE (0/low) */
+          occ_player <= (occ_player) & (~sel_pos);
         end else game_state <= game_state;
       end else if (game_state == `GAME_ST_CHECK_X) begin
-        if (~valid) game_state == `GAME_ST_ERR_X
-        else if (result == `WIN_ST_WINX) game_state <= `GAME_ST_WIN_X;
-        else if (result == `WIN_ST_CATS) game_state <= `GAME_ST_CATS;
-        else if (result == `WIN_ST_NONE) game_state <= `GAME_ST_TURN_O;
+        if (~valid_move) game_state <= `GAME_ST_ERR_X;
+        else if (result == `RESULT_WINX) game_state <= `GAME_ST_WIN_X;
+        else if (result == `RESULT_CATS) game_state <= `GAME_ST_CATS;
+        else if (result == `RESULT_NONE) game_state <= `GAME_ST_TURN_O;
         else game_state <= game_state;
       end else if (game_state == `GAME_ST_CHECK_O) begin
-        if (~valid) game_state == `GAME_ST_ERR_O)
-        else if (result == `WIN_ST_WINO) game_state <= `GAME_ST_WIN_O;
-        else if (result == `WIN_ST_CATS) game_state <= `GAME_ST_CATS;
-        else if (result == `WIN_ST_NONE) game_state <= `GAME_ST_TURN_X;
+        if (~valid_move) game_state <= `GAME_ST_ERR_O;
+        else if (result == `RESULT_WINO) game_state <= `GAME_ST_WIN_O;
+        else if (result == `RESULT_CATS) game_state <= `GAME_ST_CATS;
+        else if (result == `RESULT_NONE) game_state <= `GAME_ST_TURN_X;
         else game_state <= game_state;
       end else if (game_state == `GAME_ST_WIN_X) begin
         game_state <= game_state;
@@ -87,8 +112,8 @@ module game(turnX, turnO, occ_pos, game_st, reset, clk, flash_clk, sel_pos, butt
     end
   end
 
-  wire result[1:0];
-  check_win checker (result, occ_square, occ_player);
+  wire [1:0] result;
+  check_win _check_win (result, occ_square, occ_player);
 
 endmodule
 
@@ -98,14 +123,10 @@ endmodule
  */
 /* First bit being high ==> there's a winner. If there's a winner, second bit
  * says who */
-`define WIN_ST_NONE 2'd0
-`define WIN_ST_CATS 2'd1
-`define WIN_ST_WIN0 2'd2
-`define WIN_ST_WINX 2'd3
 module check_win(result, occ_square, occ_player);
-  output result[1:0]; //output state
-  input occ_square[8:0]; //is there an X or O here?
-  input occ_player[8:0]; //if so, which one is it.
+  output reg [1:0] result; //output state
+  input [8:0] occ_square; //is there an X or O here?
+  input [8:0] occ_player; //if so, which one is it.
 
   /* The grid looks like this:
    * 8 | 7 | 6
@@ -119,8 +140,8 @@ module check_win(result, occ_square, occ_player);
    * 852, 741, 630, 876, 543, 210, 840, 642
    * Check for a victory in each trey
    */
-  wire trey_winner[7:0];
-  wire trey_player[7:0];
+  wire [7:0] trey_winner;
+  wire [7:0] trey_player;
   check_trey col0 (trey_winner[0], trey_player[0], {occ_square[8], occ_square[5], occ_square[2]}, {occ_player[8], occ_player[5], occ_player[2]});
   check_trey col1 (trey_winner[1], trey_player[1], {occ_square[7], occ_square[4], occ_square[1]}, {occ_player[7], occ_player[4], occ_player[1]});
   check_trey col2 (trey_winner[2], trey_player[2], {occ_square[6], occ_square[3], occ_square[0]}, {occ_player[6], occ_player[3], occ_player[0]});
@@ -130,16 +151,17 @@ module check_win(result, occ_square, occ_player);
   check_trey dag0 (trey_winner[6], trey_player[6], {occ_square[8], occ_square[4], occ_square[0]}, {occ_player[8], occ_player[4], occ_player[0]});
   check_trey dag1 (trey_winner[7], trey_player[7], {occ_square[6], occ_square[4], occ_square[2]}, {occ_player[6], occ_player[4], occ_player[2]});
 
-  assign wire isWinner = (& trey_winner);
-  assign wire winningPlayer = (| trey_player);
+  wire isWinner, winningPlayer;
+  assign isWinner = (& trey_winner);
+  assign winningPlayer = (| trey_player);
 
   always @(*) begin
     if (isWinner) begin
       result <= {isWinner, winningPlayer};
     end else if (& occ_square) begin
-      result <= `WIN_ST_CATS;
+      result <= `RESULT_CATS;
     end else begin
-      result <= `WIN_ST_NONE;
+      result <= `RESULT_NONE;
     end
   end
 endmodule
@@ -149,11 +171,12 @@ endmodule
 module check_trey(win, player, occ_square, occ_player);
   output win; //was there a winner in this trey?
   output player; //who won?
-  input occ_square[2:0]; // is there an X or an O in this spot
-  input occ_player[2:0]; // if so, which one is it
+  input [2:0] occ_square; // is there an X or an O in this spot
+  input [2:0] occ_player; // if so, which one is it
 
   /* Is the same player in all squares of this trey? */
   //assign samePlayer = (occ_player[0] == occ_player[1]) & (occ_player[0] == occ_player[2]);
+  wire samePlayer;
   assign samePlayer = (& occ_player) == (| occ_player);
 
   /* If so, and all the squares are occupied (meaning the occ_player data isn't
@@ -169,57 +192,59 @@ endmodule
 module check_valid_move(valid, occ_square, sel_pos);
   output valid;
 
-  input occ_square[8:0];
-  input sel_pos[8:0];
-  input game_state[3:0];
+  input [8:0] occ_square;
+  input [8:0] sel_pos;
+  input [3:0] game_state;
 
   /* Only place a tile in an unoccupied square */
-  assign wire isUnoccupied = (occ_square & sel_pos) == 9'b000000000;
+  wire isUnoccupied;
+  assign isUnoccupied = (occ_square & sel_pos) == 9'b000000000;
 
   /* Only place 1 tile at a time */
-  assign wire isOneHot = sel_pos[0] + sel_pos[1] + sel_pos[2] + sel_pos[3] + sel_pos[4] + sel_pos[5] + sel_pos[6] + sel_pos[7] + sel_pos[8]) == 4'b0001;
+  wire isOneHot;
+  assign isOneHot = (sel_pos[0] + sel_pos[1] + sel_pos[2] + sel_pos[3] + sel_pos[4] + sel_pos[5] + sel_pos[6] + sel_pos[7] + sel_pos[8]) == 4'b0001;
 
   assign valid = isUnoccupied & isOneHot;
 endmodule
 
-`define ASCII_X 7'b01011000
-`define ASCII_O 7'b01001111
-`define ASCII_C 7'b01000011
-`define ASCII_E 7'b01000101
-`define ASCII_NONE 7'b01101110
+`define ASCII_X 8'b01011000
+`define ASCII_O 8'b01001111
+`define ASCII_C 8'b01000011
+`define ASCII_E 8'b01000101
+`define ASCII_NONE 8'b01101110
 /* Driver between internal game state and ASCII output */
-module game_st_driver (game_st, game_state);
-  output game_st[7:0];
-  input game_state[3:0];
+module game_st_driver (game_st_ascii, game_st);
+  output reg [7:0] game_st_ascii;
+  input [3:0] game_st;
   always @(*) begin
-    case (game_state)
-      `GAME_ST_WIN_X: game_st <= `ASCII_X;
-      `GAME_ST_WIN_O: game_st <= `ASCII_O;
-      `GAME_ST_CATS: game_st <= `ASCII_C;
+    case (game_st)
+      `GAME_ST_WIN_X: game_st_ascii <= `ASCII_X;
+      `GAME_ST_WIN_O: game_st_ascii <= `ASCII_O;
+      `GAME_ST_CATS: game_st_ascii <= `ASCII_C;
       `GAME_ST_ERR_X,
-      `GAME_ST_ERR_O: game_st <= `ASCII_E;
-      default: game_st <= `ASCII_NONE;
+      `GAME_ST_ERR_O: game_st_ascii <= `ASCII_E;
+      default: game_st_ascii <= `ASCII_NONE;
     endcase
   end
 endmodule
 
 module occ_pos_driver (occ_pos, occ_square, occ_player, trey_winner, flash_clk, rst);
-  output occ_pos[8:0];
-  input occ_square[8:0];
-  input occ_player[8:0];
-  input trey_winner[7:0];
+  output [8:0] occ_pos;
+  input [8:0] occ_square;
+  input [8:0] occ_player;
+  input [7:0] trey_winner;
   input rst;
   input flash_clk;
 
   /* a mask that gets applied to occ_player at a rate of 1/2 flash_clk */
-  reg occ_O_mask[8:0];
+  reg [8:0] occ_O_mask;
 
   always @(posedge flash_clk) begin
     if (rst) occ_O_mask = 9'b000000000;
     else occ_O_mask = ~occ_O_mask;
   end
 
-  wire pre_occ_pos[8:0];
+  wire [8:0] pre_occ_pos;
   assign pre_occ_pos = occ_square & (occ_player | occ_O_mask);
   /* Each occ_pos signal is 1'b0 to indicate unoccupied
    * 1'b1 to indicate occupied by X
@@ -227,10 +252,10 @@ module occ_pos_driver (occ_pos, occ_square, occ_player, trey_winner, flash_clk, 
   /* Only for occupied tiles, display high for all the X's, and only display
    * high for the Os when occ_O_mask is high */
 
-  wire flash_clk_mask[8:0];
-  assign wire flash_clk_mask = {flash_clk, flash_clk, flash_clk, flash_clk, flash_clk, flash_clk, flash_clk, flash_clk, flash_clk};
+  wire [8:0] flash_clk_mask;
+  assign flash_clk_mask = {flash_clk, flash_clk, flash_clk, flash_clk, flash_clk, flash_clk, flash_clk, flash_clk, flash_clk};
 
-  reg trey_mask[8:0];
+  reg [8:0] trey_mask;
   always @(*) begin
     if (trey_winner[0]) trey_mask <= 9'b110110110; //852
     else if (trey_winner[1]) trey_mask <= 9'b101101101; //741
@@ -248,42 +273,29 @@ module occ_pos_driver (occ_pos, occ_square, occ_player, trey_winner, flash_clk, 
    * flash_clk_mask alters between 000111111 and 111111111. */
 endmodule
 
-/* TODO, do we wanna debounce, or do we wanna only detect posedge... */
-module debouncer (btn_posedge, btn, clk_in, rst);
-  parameter POW = 16; /* downsample the clock by 2^POW */
-  output reg btn_posedge;
-  input btn, clk_in, rst;
+/* A 2-bit debouncer is basically just a posedge detector */
+/* To properly use this as a debouncer, we might wanna have more than 8 bits
+ * in the shift register. Or, we could downsample the clock */
+module debouncer(data_out, data_in, clk_in, reset);
+  input data_in, clk_in, reset;
+  output data_out;
 
-  reg clk_en, clk_en_d; /* slower clocks - downsampled */
-  reg [2:0] step_d;		/* state of button press - stepd[2] is most recent */
-  reg [POW:0] clk_dv;	/* counter buffer to divide the clock */
-  wire [POW+1:0] clk_dv_inc;	/* counter buffer with overflow - used to pulse downsampled clock */
+  reg [3:0] q;
 
-  assign clk_dv_inc = clk_dv + 1;
-  always @ (posedge clk_in) begin
-    if (rst) begin
-      clk_dv <= 0;
-      clk_en <= 0;
-      clk_en_d <= 0;
+  always @ ( posedge clk_in or posedge reset) begin
+
+    if (reset == 1'b1) begin
+      q <= 8'b0;
     end else begin
-      clk_dv <= clk_dv_inc[POW:0]; /* increment counter */
-      clk_en <= clk_dv_inc[POW+1]; /* the overflow bit in the counter IS the clock tick - simply downsample by a factor of 2 */
-      clk_en_d <= clk_en; /* delay this downsampled clock by 1 tick */
+      /* Shift all the bits one to the right. Shift in "data_in" */
+      q[0] <= data_in;
+      q[1] <= q[0];
+      q[2] <= q[1];
+      q[3] <= q[2];
     end
   end
 
-  always @ (posedge clk_in) begin
-    if (rst) step_d[2:0] <= 0;
-    else if (clk_en) step_d[2:0] <= {btn, step_d[2:1]};
-  end
-
-  // Detecting posedge of btn
-  wire is_btn_posedge;
-  assign is_btn_posedge = ~ step_d[0] & step_d[1];
-  always @ (posedge clk_in) begin
-    if (rst) btn_posedge <= 0;
-    else if (clk_en_d) btn_posedge <= is_btn_posedge;
-    else btn_posedge <= 0;
-  end
-
+  /* If the oldest data is low and ALL of the newest datas are high, then we
+   * have a button press */
+  assign data_out = !q[3] & q[2] & q[1] & q[0];
 endmodule
